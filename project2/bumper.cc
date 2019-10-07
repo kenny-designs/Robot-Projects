be372d5091a1ed5a759b2ec519707f9adc217667
Alexander Aguilar <alexanderman2004@yahoo.com>
2019-10-07 10:41:20 -0400

    refactored bumper.cc code for make it easier to read

--- project2/bumper.cc
+++ project2/bumper.cc
@@ -1,52 +1,90 @@
 #include "Robot.h"
-#include "Vector2.h"
-#include <vector>
+#include "Vector2.h" // struct that holds x and y coordinates
+#include <vector>    // holding a series of waypoints
+
+// forward declarations
+void wallWorld(Robot& robot);
+void mazeWorld(Robot& robot);
+void traverseWaypoints(std::vector<Vector2>& waypoints, Robot& robot);
 
 int main(int argc, char *argv[])
 {  
-  //Waypoints for World 1
-  std::vector<Vector2> waypointsVector1;
-
-  waypointsVector1.push_back(Vector2(0,0));
-  waypointsVector1.push_back(Vector2(10,3));
-  waypointsVector1.push_back(Vector2(10,6));
-  waypointsVector1.push_back(Vector2(9,9));
-  waypointsVector1.push_back(Vector2(4,10));
-  waypointsVector1.push_back(Vector2(1,6));
-  waypointsVector1.push_back(Vector2(0,0));
-
-  //Waypoints for World 2
-  std::vector<Vector2> waypointsVector2;
-
-  waypointsVector2.push_back(Vector2(0,0));
-  waypointsVector2.push_back(Vector2(12,1));
-  waypointsVector2.push_back(Vector2(12,4));
-  waypointsVector2.push_back(Vector2(1,4));
-  waypointsVector2.push_back(Vector2(1,8));
-  waypointsVector2.push_back(Vector2(12,8));
-  waypointsVector2.push_back(Vector2(12,12));
-  waypointsVector2.push_back(Vector2(0,12));
-
   // Initialize our robot with isSimulation set to true
   Robot robot;
 
   // Enable motors
   robot.setMotorEnable(true);
 
-  // Iterate through World 1 Waypoints
-  for (int i = 0; i < waypointsVector1.size(); i++)
-  {
-    robot.moveToWaypoint(waypointsVector1[i]);
-  }
+  // Traverse wall world
+  // wallWorld(robot);
 
-  // Iterate through World 2 Waypoints
-  /*
-  for (int i = 0; i < waypointsVector2.size(); i++)
-  {
-    std::cout << waypointsVector2[i].x << "," << waypointsVector2[i].y << "\n";
-
-    robot.moveToWaypoint(waypointsVector2[i]);
-  }
-  */
+  // Traverse maze world
+  mazeWorld(robot);
 }
 
+/**
+ * Generates waypoints for the robot to follow to solve the
+ * wall-world.png level. In particular, it will complete a circuit
+ * around the blob in the center of the world and end up where it
+ * started.
+ *
+ * @param robot - the robot that will be solving the level
+ */ 
+void wallWorld(Robot& robot)
+{
+  // vector to hold all waypoints for the robot to travel to
+  std::vector<Vector2> waypoints;
+
+  // populate vector with waypoints
+  waypoints.push_back(Vector2( 0,  0));
+  waypoints.push_back(Vector2(10,  3));
+  waypoints.push_back(Vector2(10,  6));
+  waypoints.push_back(Vector2( 9,  9));
+  waypoints.push_back(Vector2( 4, 10));
+  waypoints.push_back(Vector2( 1,  6));
+  waypoints.push_back(Vector2( 0,  0));
+
+  // have robot travel to each waypoint
+  traverseWaypoints(waypoints, robot);
+}
+
+/**
+ * Generates waypoints for the robot to follow to solve the
+ * maze.png level. In particular, it will traverse the maze from
+ * the bottom left-hand corner to the very top left-hand corner.
+ *
+ * @param robot - the robot that will be solving the level
+ */ 
+void mazeWorld(Robot& robot)
+{
+  // vector to hold all waypoints for the robot to travel to
+  std::vector<Vector2> waypoints;
+
+  // populate vector with waypoints
+  waypoints.push_back(Vector2( 0,  0));
+  waypoints.push_back(Vector2(12,  1));
+  waypoints.push_back(Vector2(12,  4));
+  waypoints.push_back(Vector2( 1,  4));
+  waypoints.push_back(Vector2( 1,  8));
+  waypoints.push_back(Vector2(12,  8));
+  waypoints.push_back(Vector2(12, 12));
+  waypoints.push_back(Vector2( 0, 12));
+
+  // have robot travel to each waypoint
+  traverseWaypoints(waypoints, robot);
+}
+
+/**
+ * Makes the given robot travel along a series of Vector2 waypoints
+ * starting from the first element and ending with the last.
+ *
+ * @param waypoints - a series of waypoints for the robot to travel to
+ * @param robot     - the robot that will be traveling to each waypoint
+ */
+void traverseWaypoints(std::vector<Vector2>& waypoints, Robot& robot)
+{
+  for (int i = 0; i < waypoints.size(); i++)
+  {
+    robot.moveToWaypoint(waypoints[i]);
+  }
+}
