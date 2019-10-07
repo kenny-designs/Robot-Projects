20ec0495fd96857415d3de04a34755276f18a0e3
Alexander Aguilar <alexanderman2004@yahoo.com>
2019-10-07 15:28:53 -0400

    further fleshed out methods for movement

--- project3/blobs.cc
+++ project3/blobs.cc
@@ -7,9 +7,10 @@ using namespace PlayerCc;
 
 // forward declarations
 bool isRedBlob(player_blobfinder_blob_t& blob);
+bool isFacingBeacon(player_blobfinder_blob_t& blob);
 void printBlob(player_blobfinder_blob_t& blob);
-void rotateToFaceBeacon(Position2dProxy& pp, player_blobfinder_blob_t& blob);
-void moveToBeacon(Position2dProxy& pp, player_blobfinder_blob_t& blob);
+double rotateToFaceBeacon(Position2dProxy& pp, player_blobfinder_blob_t& blob);
+double moveToBeacon(Position2dProxy& pp, player_blobfinder_blob_t& blob);
 
 int main(int argc, char *argv[])
 {  
@@ -23,7 +24,7 @@ int main(int argc, char *argv[])
   // Variables
   player_blobfinder_data_t  blobList;   // Structure holding the blobs found
   player_blobfinder_blob_t* theBlobs;   // A point to a list of blobs
-  double turnrate = 0, speed = 0;
+  double velocity, angularVelocity;
 
   // Allow the program to take charge of the motors (take care now)
   pp.SetMotorEnable(true);
@@ -31,6 +32,10 @@ int main(int argc, char *argv[])
   // Control loop
   while(true) 
   {
+    // for safety, velocity and angularVelocity are 0 by default
+    velocity = 0;
+    angularVelocity = 0;
+
     // Read from the proxies
     robot.Read();
     blobfinder.Read();
@@ -38,25 +43,36 @@ int main(int argc, char *argv[])
     // We only want to drive if the bumpers are not pressed
     if (!(bp[0] || bp[1])) continue;
 
-    // Iterate over blobs if any
+    // Iterate over blobs if any. Only process the first red blob we see
     for (int i = 0; i < bf.GetCount(); i++)
     {
-      // if a red blob is found, process then break
+      // if not a red blob, check the next element
       player_blobfinder_blob_t blob = bf.GetBlob(i);
-      if (isRedBlob(blob))
+      if (!isRedBlob(blob)) continue;
+
+      // found a red blob! print it to the screen
+      printBlob(blob);
+
+      // if not facing the beacon, rotate towards it
+      if (!isFacingBeacon(blob))
       {
-        printBlob(blob);
-        break;
+        angularVelocity = rotateToFaceBeacon(pp, blob);
       }
+      // if the robot is facing the beacon, begin moving towards it
+      else
+      {
+        velocity = moveToBeacon(pp, blob);
+      }
+
+      // only need to process a single red blob, break
+      break;
     }
 
-    std::cout << "Speed:     " << speed    << "\n";
-    std::cout << "Turn rate: " << turnrate << "\n\n";
+    std::cout << "Velocity:        " << velocity        << "\n";      
+    std::cout << "AngularVelocity: " << angularVelocity << "\n\n";
 
-    // For safety, in the absence of a controller, do nothing.
-    speed = 0;
-    turnrate = 0;
-    pp.SetSpeed(speed, turnrate);
+    // move to the beacon
+    pp.SetSpeed(velocity, angularVelocity);
   }
 }
 
@@ -64,6 +80,13 @@ int main(int argc, char *argv[])
 bool isRedBlob(player_blobfinder_blob_t& blob)
 {
   return ((short)blob.color) == 0;
+}
+
+/** Returns true if we are currently facing the beacon */
+bool isFacingBeacon(player_blobfinder_blob_t& blob)
+{
+  // TODO: find what values constitue as 'facing' the beacon
+  return false;
 }
 
 /** Print blob data */
@@ -77,15 +100,33 @@ void printBlob(player_blobfinder_blob_t& blob)
   std::cout << "\n";
 }
 
-/** Rotate to face the beacon */
-void rotateToFaceBeacon(Position2dProxy& pp, player_blobfinder_blob_t& blob)
+/** Returns the appropriate angular velocity needed to face the beacon */
+double rotateToFaceBeacon(Position2dProxy& pp, player_blobfinder_blob_t& blob)
 {
-  //
+  if (isFacingBeacon(blob)) return 0;
+
+  // TODO: fill this in with actual numbers
+  uint32_t left = 0, right = 0;
+  if (blob.x < left)
+  {
+    // right to the right
+  }
+  else if (blob.x > right)
+  {
+    // rotate to the left
+  }
 }
 
-/** Move to the beacon within 2ft */
-void moveToBeacon(Position2dProxy& pp, player_blobfinder_blob_t& blob)
+/** Returns the appropriate velocity needed to reach the beacon */
+double moveToBeacon(Position2dProxy& pp, player_blobfinder_blob_t& blob)
 {
-  //
+  // TODO: check minimum area must be for us to consider the movement complete
+  // if already within 2ft, stop
+  if (blob.area <= 20) return 0;
+
+  // by default, we move at 0.1 m/s
+  double vel = 0.1;
+
+  return vel;
 }
 
