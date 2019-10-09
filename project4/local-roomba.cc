d5d3b2d162f16fc821c1451fdfd9fba76464d1b6
Alexander Aguilar <alexanderman2004@yahoo.com>
2019-10-09 17:36:19 -0400

    Cleaned up local-roomba.cc

--- project4/local-roomba.cc
+++ project4/local-roomba.cc
@@ -1,131 +1,101 @@
 /**
- * local-roomba.cc
- * 
- * Sample code for a roomba-like robot that has two front bumpers and
- * magically knows where it is. 
- *
- * Written by: Simon Parsons
- * Date:       24th October 2011
- *  
- **/
-
+ * Proj4 Part1
+ * Group10: Aguilar, Andrew, Kamel, Kennedy
+ */
 
 #include <iostream>
 #include <libplayerc++/playerc++.h>
 using namespace PlayerCc;  
 
-/**
- * Function headers
- *
- **/
-
+// Forward declarations
 player_pose2d_t readPosition(LocalizeProxy& lp);
 void printRobotData(BumperProxy& bp, player_pose2d_t pose);
 
-/**
- * main()
- *
- **/
-
 int main(int argc, char *argv[])
-{  
-
-
+{
   // Variables
   int counter = 0;
-  double speed;            // How fast do we want the robot to go forwards?
-  double turnrate;         // How fast do we want the robot to turn?
-  player_pose2d_t  pose;   // For handling localization data
+  double speed;         // How fast do we want the robot to go forwards?
+  double turnrate;      // How fast do we want the robot to turn?
+  player_pose2d_t pose; // For handling localization data
 
-  // Set up proxies. These are the names we will use to connect to 
-  // the interface to the robot.
+  // Proxies
   PlayerClient    robot("localhost");  
   BumperProxy     bp(&robot,0);  
   Position2dProxy pp(&robot,0);
   LocalizeProxy   lp (&robot, 0);
 
-  // Allow the program to take charge of the motors (take care now)
+  // Enable motor
   pp.SetMotorEnable(true);
 
   // Main control loop
   while(true) 
-    {    
-      // Update information from the robot.
-      robot.Read();
-      // Read new information about position
-      pose = readPosition(lp);
+  {    
+    // Update information from the robot.
+    robot.Read();
 
-      // Print data on the robot to the terminal
-      printRobotData(bp, pose);
+    // Read new information about position
+    pose = readPosition(lp);
 
-      // This part of the code should be very familiar by now.
-      //
-      // If either bumper is pressed, stop. Otherwise just go forwards
+    // Print data on the robot to the terminal
+    printRobotData(bp, pose);
 
-      if(bp[0] || bp[1]){
-	speed= 0;
-	turnrate= 0;
-      } 
-      else {
-	speed=.1;
-        turnrate = 0;
-      }     
-
-      // What are we doing?
-      std::cout << "Speed: " << speed << std::endl;      
-      std::cout << "Turn rate: " << turnrate << std::endl << std::endl;
-
-      // Send the commands to the robot
-      pp.SetSpeed(speed, turnrate);  
-      // Count how many times we do this
-      counter++;
+    if(bp[0] || bp[1])
+    {
+      speed    = 0;
+      turnrate = 0;
     }
-  
-} // end of main()
+    else
+    {
+      speed    = 0.1;
+      turnrate = 0;
+    }     
+
+    // What are we doing?
+    std::cout << "Speed:     " << speed << std::endl;      
+    std::cout << "Turn rate: " << turnrate << std::endl << std::endl;
+
+    // Send the commands to the robot
+    pp.SetSpeed(speed, turnrate);  
+
+    // Count how many times we do this
+    counter++;
+  }
+
+}
 
 
 /**
- * readPosition()
- *
  * Read the position of the robot from the localization proxy. 
  *
  * The localization proxy gives us a hypothesis, and from that we extract
  * the mean, which is a pose. 
- *
- **/
-
+ */
 player_pose2d_t readPosition(LocalizeProxy& lp)
 {
-
   player_localize_hypoth_t hypothesis;
   player_pose2d_t          pose;
   uint32_t                 hCount;
 
-  // Need some messing around to avoid a crash when the proxy is
-  // starting up.
-
+  // Need some messing around to avoid a crash when the proxy is starting up.
   hCount = lp.GetHypothCount();
 
-  if(hCount > 0){
+  if(hCount > 0)
+  {
     hypothesis = lp.GetHypoth(0);
     pose       = hypothesis.mean;
   }
 
   return pose;
-} // End of readPosition()
+}
 
 
 /**
- *  printRobotData
- *
  * Print out data on the state of the bumpers and the current location
  * of the robot.
- *
- **/
-
+ */
 void printRobotData(BumperProxy& bp, player_pose2d_t pose)
 {
-
   // Print out what the bumpers tell us:
   std::cout << "Left  bumper: " << bp[0] << std::endl;
   std::cout << "Right bumper: " << bp[1] << std::endl;
@@ -135,6 +105,4 @@ void printRobotData(BumperProxy& bp, player_pose2d_t pose)
   std::cout << "X: " << pose.px << std::endl;
   std::cout << "Y: " << pose.py << std::endl;
   std::cout << "A: " << pose.pa << std::endl;
-
-  
-} // End of printRobotData()
+}
