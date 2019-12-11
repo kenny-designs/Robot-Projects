c589cfc06decf6bb55f3128f7d0c4b497ad7429b
matt <mattlol85@gmail.com>
2019-12-10 20:17:58 -0500

    Translated methods from sample code to Java

--- project6/MapPlanner.java
+++ project6/MapPlanner.java
@@ -4,33 +4,79 @@ import java.util.ArrayList;
 import java.io.*;
 
 public class MapPlanner {
+	final static int MAX_SIZE = 32;
+
 	public static void main(String[] args) {
-		final int MAX_SIZE = 32;
+
 		int[][] oGrid = new int[MAX_SIZE][MAX_SIZE];
-		
+		readMap(oGrid);
+		printMap(oGrid);
+		writeMap(oGrid);
 	}
-	// Read a map in from the file map.txt
+
+	static // Read a map in from the file map.txt
 	void readMap(int[][] oMap) {
-		
+		File mapFile = new File("map.txt");
+		try {
+			Scanner in = new Scanner(mapFile);
+			for (int i = MAX_SIZE - 1; i >= 0; i--) {
+				for (int j = 0; j < MAX_SIZE; j++) {
+					oMap[i][j] = in.nextInt();
+				}
+
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found. Exiting");
+			System.exit(0);
+			e.printStackTrace();
+		}
 	}
-	// Write a map out to the file map-out.txt
+
+	static // Write a map out to the file map-out.txt
 	void writeMap(int[][] oMap) {
-		
+		File mapFile = new File("map-out.txt");
+		try {
+			PrintWriter printer = new PrintWriter(mapFile);
+
+			for (int i = MAX_SIZE - 1; i >= 0; i--) {
+				for (int j = 0; j < MAX_SIZE; j++) {
+					printer.print(oMap[i][j] + " ");
+				}
+				printer.println();
+			}
+
+			printer.close();
+
+		} catch (FileNotFoundException e) {
+			// Should this ever be caught?
+			// Probably not?
+			e.printStackTrace();
+		}
 	}
-	// Print the map on the screen
+
+	static // Print the map on the screen
 	void printMap(int[][] oMap) {
-		
+		for (int i = MAX_SIZE - 1; i >= 0; i--) {
+			for (int j = 0; j < MAX_SIZE; j++) {
+				System.out.print(oMap[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
+
 	void readPlanLength() {
-		
+
 	}
+
 	void readPlan() {
-		
+
 	}
+
 	void printPlan() {
-		
+
 	}
+
 	void writePlan() {
-		
+
 	}
 }
