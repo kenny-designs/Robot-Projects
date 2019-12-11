2a77119acd82ad32c11a3aca9054071c027c26c3
matt <mattlol85@gmail.com>
2019-12-10 19:51:46 -0500

    Added Java MapPlanner

---
+++ project6/MapPlanner.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.util.AbstractCollection;
+import java.util.ArrayList;
+import java.io.*;
+
+public class MapPlanner {
+	public static void main(String[] args) {
+		final int MAX_SIZE = 32;
+		int[][] oGrid = new int[MAX_SIZE][MAX_SIZE];
+		
+	}
+	// Read a map in from the file map.txt
+	void readMap(int[][] oMap) {
+		
+	}
+	// Write a map out to the file map-out.txt
+	void writeMap(int[][] oMap) {
+		
+	}
+	// Print the map on the screen
+	void printMap(int[][] oMap) {
+		
+	}
+	void readPlanLength() {
+		
+	}
+	void readPlan() {
+		
+	}
+	void printPlan() {
+		
+	}
+	void writePlan() {
+		
+	}
+}
